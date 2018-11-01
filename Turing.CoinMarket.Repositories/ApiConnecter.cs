8b7528bc5414984dba814dac19709ef6ce59014e
Michail Knyrevic <michail@webilize.com>
2018-11-01 15:04:05 +0100

    Added Loader and refresh button.

--- Turing.CoinMarket.Repositories/ApiConnecter.cs
+++ Turing.CoinMarket.Repositories/ApiConnecter.cs
@@ -8,7 +8,7 @@ namespace Turing.CoinMarket.Repositories
     public class ApiConnecter
     {
         public const string CoinMarketBase = "https://api.coinmarketcap.com";
-        public const string TickerUrl = "/v2/ticker/?sort=id&structure=array&convert={0}&start={1}&limit={2}";
+        public const string TickerUrl = "/v2/ticker/?sort=symbol&structure=array&convert={0}&start={1}&limit={2}";
 
         public static async Task<string> GetApiResult(string urlParams)
         {
--- Turing.CoinMarket.Test/MainWindow.xaml.cs
+++ Turing.CoinMarket.Test/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
+using FontAwesome.WPF;
 using Turing.CoinMarket.Repositories;
 
 namespace Turing.CoinMarket.Test.UI
@@ -17,16 +19,30 @@ namespace Turing.CoinMarket.Test.UI
         public MainWindow()
         {
             Loaded += Window_Loaded;
-            InitializeComponent();
 
-            CbxCurrency.ItemsSource = new List<string> { "USD", "EUR", "BTC" };
             _pager = new Pager();
             this._coinMarketCapQuery = new CoinMarketCapQuery();
+
+            InitializeComponent();            
+        }
+
+        private async void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            await LoadPage();
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            this.cryptoCurrencyGrid.ItemsSource = await _coinMarketCapQuery.GetAll();
+            await LoadPage();
+            InitTimer();
+        }
+
+        private void InitTimer()
+        {
+            var dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(10);            
+            dispatcherTimer.Start();
         }
 
         private async void Next_Click(object sender, RoutedEventArgs e)
@@ -47,8 +63,23 @@ namespace Turing.CoinMarket.Test.UI
 
         private async Task LoadPage()
         {
-            var request = RequesetFactory.New(_pager, CbxCurrency.Text);
-            this.cryptoCurrencyGrid.ItemsSource = await _coinMarketCapQuery.GetAll(request);
+            LoadingSpinner.Icon = FontAwesomeIcon.Spinner;
+            LoadingSpinner.Spin = true;
+            try
+            {
+                var request = RequesetFactory.New(_pager, (CbxCurrency.SelectedItem as ComboBoxItem).Content.ToString());
+                this.CryptoCurrencyGrid.ItemsSource = await _coinMarketCapQuery.GetAll(request);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                LoadingSpinner.Icon = FontAwesomeIcon.Check;
+                LoadingSpinner.Spin = false;
+            }                                    
         }
 
         private void DisablePreviousIfNeeded()
@@ -74,6 +105,10 @@ namespace Turing.CoinMarket.Test.UI
 
         private async void CbxCurrency_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (!IsLoaded)
+            {
+                return;
+            }
             await LoadPage();
         }
     }
